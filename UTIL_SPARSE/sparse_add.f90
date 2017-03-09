825b57e54b6df2f74fe0cce9cd83788bfcffa76b
fireball-QMD <elius.jom@gmail.com>
2017-03-09 12:21:47 +0100

    gplv3

--- UTIL_SPARSE/sparse_add.f90
+++ UTIL_SPARSE/sparse_add.f90
@@ -18,11 +18,21 @@
 ! Ohio University - Dave Drabold
 
 !
-! RESTRICTED RIGHTS LEGEND
-! Use, duplication, or disclosure of this software and its documentation
-! by the Government is subject to restrictions as set forth in subdivision
-! { (b) (3) (ii) } of the Rights in Technical Data and Computer Software
-! clause at 52.227-7013.
+! fireball-qmd is a free (GPLv3) open project.
+
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 ! sparse_add.f90
 ! Program Description
