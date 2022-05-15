91dffa21aa732b5b71397937d41e5176deb54063
dgtrabada <dgtrabada@gmail.com>
2022-05-15 11:28:39 +0000

    f2py

--- pyfb/f2py/libf2py.f90
+++ pyfb/f2py/libf2py.f90
@@ -1,7 +1,5 @@
 program libf2py
 print*, 'Wrapping Fortran Python using F2PY'
-!call load_base()
-!call load_data()
 end 
 
 subroutine start()
@@ -13,28 +11,52 @@ subroutine start()
   write (*,*) ' FIREBALL RUNTIME : ',time_end-time_begin,'[sec]'
 end
 
-subroutine do_initbasics()
-!   call initbasics ()
+subroutine f2py_initbasics()
+   ! more or less initbasics ()
+   use options
+   use configuration
+   use interactions
+   use scf
+   use integrals
+   use outputs
+   use kpoints
+   use optimization
+   use md
+   use charges
+   use barrier
+   use transport
+   use energy
+   use neighbor_map
+   use forces
+   use mpi_main
+   use hartree_fock
+
+   implicit none
+! Local Variable Declaration and Description
+! ===========================================================================
+   integer iatom
+   integer in1
+   integer icount
+   integer counter
+   integer counter_ini
+   integer isorp
+   integer ideriv
+   integer issh
+   integer numorbPP_max
+   integer numorb
+   integer l
+   integer imu
+
+   real distance
+   real, dimension (3) :: vector
+
+   logical file_exists
    call welcome
    call initconstants (sigma, sigmaold, scf_achieved)
    call diagnostics (ioff2c, ioff3c, itestrange, testrange)
    call readparam ()
    call readinfo ()
-   ! call initconstants (sigma, sigmaold, scf_achieved) 
-   ! Read the parameter file - fireball.param
-   ! call readparam () 
-   ! Read the info.dat
-   ! call readinfo ()  
-   ! Read data from the basis file - XXX.bas.
-   ! call readbasis 
-   ! call readlvs 
-   ! call readfragments ()
-   ! call initmasses 
-   ! call readquench 
-   ! call readbarrier 
-   ! call make_munu (nspecies)
-   ! call make_munuPP (nspecies)
-   ! ....
+   if( iclassicMD > 0 ) call readdata_classicMD ()
 
 end
 
